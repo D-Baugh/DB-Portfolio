a96d0ff94777e2d918b5a2b4b3b166ef6eff266a
D-Baugh <deshan_baugh@yahoo.com>
2024-11-03 03:06:23 -0800

    javascript with button press api call added

---
+++ dev/BaughDeshan-Profile/js/script.js
@@ -0,0 +1,56 @@
+(() => {
+     //  Build DOM
+  // create elements
+  scriptSection = document.createElement("section");
+  scriptSectionHeader = document.createElement("h2");
+  scriptP = document.createElement("p")
+  setup = document.createElement("p")
+  delivery = document.createElement("p")
+  funnyButton = document.createElement("button");
+//   assign id attributes
+  scriptSection.setAttribute("id", "scriptFunnies");
+  scriptSectionHeader.setAttribute("id", "scriptHeader");
+  scriptP.setAttribute("id", "scriptP")
+  setup.setAttribute("id","setupP")
+  delivery.setAttribute("id"," deliveryP")
+  // assign inner html
+  scriptP.innerHTML = "This section calls the jokeapi and outputs some funnies!"
+  scriptSectionHeader.innerHTML = "JavaScript Games & Funnies";
+  funnyButton.innerHTML = "Click for a new funny!"
+  // append to body
+  document.body.append(scriptSection)
+  document.body.append(scriptSectionHeader);
+  document.body.append(scriptP)
+  document.body.append(setup)
+  document.body.append(delivery)
+  document.body.append(funnyButton)
+
+  // store api within variable URL
+  const url = `https://v2.jokeapi.dev/joke/Programming?blacklistFlags=nsfw,religious,political,racist,sexist,explicit&type=twopart`;
+
+  const handleError = (error) => {
+    console.error("API has run into an error", error);
+  };
+  const handleApi = (api) => {
+    const ssetup = api.setup;
+    const ddelivery = api.delivery;
+
+    setup.innerHTML = ssetup;
+    delivery.innerHTML = ddelivery;
+
+    console.log(handleApi);
+  };
+  document.querySelector("button").addEventListener("click", () => {
+    fetchAPI();
+  });
+  const fetchAPI = () => {
+    fetch(url)
+      // convert api data into usable object data
+      .then((response) => response.json())
+      //
+      .then((json) => handleApi(json))
+      .catch((error) => handleError(error));
+  };
+  fetchAPI();
+ 
+})();
